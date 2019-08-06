3cb357bce2a2d332daa6307c0589cd23c031f19e
Tiana Johnson <tiana.marie013@gmail.com>
2019-08-06 17:31:13 -0500

    some style

--- src/App.js
+++ src/App.js
@@ -1,42 +1,59 @@
-import React from 'react';
-
+import React, { Component } from 'react';
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 import './App.css';
 
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name:'',
+      email:'',
+      notes:'',
+    }
+  }
 
-function App() {
-
-
-  return (
-    <div className="App">
-      <header className="App-header">
-      <h1>I am attempting to spike nodemailer.</h1>
-      <Form>
-      <FormGroup>
-          <Label>Name: </Label>
-          <Input type="Name" name="email" id="exampleName" placeholder="Full Name" />
-        </FormGroup>
-      <FormGroup>
-          <Label>Email: </Label>
-          <Input type="email" name="email" id="exampleEmail" placeholder="Email Address" />
-        </FormGroup>
-        <br/>
-        <FormGroup>
-          <Input type="textarea" name="text" id="exampleText" />  
-        </FormGroup>
-      </Form>
+render(){
+return (
+  <div className="App">
+    <header className="App-header">
+    <h1>I am attempting to spike nodemailer.</h1>
+    <Form>
+    <FormGroup>
+        <Label>Name: </Label>
+        <Input type="Name" 
+               name="email" 
+               id="exampleName" 
+               placeholder="Full Name" />
+      </FormGroup>
+    <FormGroup>
+        <Label>Email: </Label>
+        <Input type="email" 
+               name="email" 
+               id="exampleEmail" 
+               placeholder="Email Address" />
+      </FormGroup>
       <br/>
-      <Button>Yeah!</Button>
-      </header>
-      <Form>
-        <Input>
-        </Input>
+      <FormGroup>
+        <Input type="textarea"
+               name="text"
+               id="exampleText"
+               style={{ height: 200, width: 300 }} />  
+      </FormGroup>
+    </Form>
+    <br/>
+    <Button>Yeah!</Button>
+    </header>
+    <Form>
+      <Input>
+      </Input>
 
-      </Form>
-      <br/>
-      <Button>Yeah!</Button>
-    </div>
-  );
+    </Form>
+    <br/>
+    <Button>Yeah!</Button>
+  </div>
+
+);
+}
 }
 
 export default App;
