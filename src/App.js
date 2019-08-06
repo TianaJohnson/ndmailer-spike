edb354ebe2e8e802d28d0cde0891f6a5916f8bd2
Tiana Johnson <tiana.marie013@gmail.com>
2019-08-06 17:10:27 -0500

    form build with reactstrap

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
+
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 import './App.css';
 
+
 function App() {
+
+
   return (
     <div className="App">
       <header className="App-header">
       <h1>I am attempting to spike nodemailer.</h1>
+      <Form>
+      <FormGroup>
+          <Label>Name: </Label>
+          <Input type="Name" name="email" id="exampleName" placeholder="Full Name" />
+        </FormGroup>
+      <FormGroup>
+          <Label>Email: </Label>
+          <Input type="email" name="email" id="exampleEmail" placeholder="Email Address" />
+        </FormGroup>
+        <br/>
+        <FormGroup>
+          <Input type="textarea" name="text" id="exampleText" />  
+        </FormGroup>
+      </Form>
+      <br/>
+      <Button>Yeah!</Button>
       </header>
+      <Form>
+        <Input>
+        </Input>
+
+      </Form>
+      <br/>
+      <Button>Yeah!</Button>
     </div>
   );
 }
