39a12d113b259a60d387074e29f0e9d63d89aaa0
Tiana Johnson <tiana.marie013@gmail.com>
2019-08-07 16:54:02 -0500

    server build

--- server/server.js
+++ server/server.js
@@ -1,12 +1,20 @@
 // Require express - gives us a function
 const express = require('express');
+const bodyParser = require('body-parser')
+const nodemailer = require('nodemailer')
 
 // Create an instance of express by calling the function returned above - gives us an object
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 3001
 
 // express static file serving - public is the folder name
 app.use(express.static('server/public'));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false}))
+
+app.post('/api/form', (req, res) => {
+    console.log(req.body)
+})
 
 // Start up our server
 app.listen(PORT, function(){
